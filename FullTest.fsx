ebc80291561e4d11e190084f227f5093b584f933
Seng Jik <853974536@qq.com>
2021-11-16 14:25:10 +0800

    FullTest can test tags

--- FullTest.fsx
+++ FullTest.fsx
@@ -19,10 +19,16 @@ let sourceName =
     |> function
         | None -> 
             printfn "Usage:"
-            printfn "    dotnet fsi FullTest.fsx <sourceName>"
+            printfn "    dotnet fsi FullTest.fsx <sourceName> [--list-posts | --list-tags] "
             printfn ""
             exit -1
         | Some x -> x
+
+
+let testTarget =
+    Environment.GetCommandLineArgs ()
+    |> Array.tryItem 3
+    |> Option.defaultValue "--list-posts"
 
 
 open QwQ
@@ -39,11 +45,22 @@ let source =
             exit -1
 
 
-Source.allPosts source
-|> AsyncSeq.map Result.unwrap
-|> AsyncSeq.iteriAsync (fun i x -> async {
-    printfn $"{Source.name source}: Page {i} has {Seq.length x} posts" })
-|> Async.RunSynchronously
+match testTarget with
+| "--list-tags" ->
+    match source with
+    | :? ITags as source ->
+        Tags.allTags source
+        |> AsyncSeq.iter (Result.unwrap >> printfn "%s")
+        |> Async.RunSynchronously
+    | _ -> printfn "%A source is not an ITags." <| Source.name source
+| "--list-posts" ->
+    Source.allPosts source
+    |> AsyncSeq.map Result.unwrap
+    |> AsyncSeq.iteriAsync (fun i x -> async {
+        printfn $"{Source.name source}: Page {i} has {Seq.length x} posts" })
+    |> Async.RunSynchronously
+| x -> printfn "Here is no test target %A." x
+
 
 
 
